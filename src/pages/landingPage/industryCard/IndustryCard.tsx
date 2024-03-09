597287c58642f0e9e1c2860c61d9e3edefe42c72
Solnyshko-eto-ya <true.solnyshko@gmail.com>
2024-03-09 18:29:08 +0300

    landing layout fixes

--- src/pages/landingPage/industryCard/IndustryCard.tsx
+++ src/pages/landingPage/industryCard/IndustryCard.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import './industryCard.css';
 
 import img1 from './images/1.png';
@@ -15,64 +14,62 @@ import img11 from './images/11.png';
 import img12 from './images/12.png';
 
 const IndustryCard = () => {
-  return (
-    <div className="card">
-
-        <div className="gridCard-container">
-
-            <div className="gridCard-items">
-                <div className="gridCard-item">
-                    <img className="gridCard-img" src={img1} alt="Косметика" />
-                    <a className="gridCard-item__link" href="http://212.233.79.177:6688/" target='_blanket' rel="noopener noreferrer"><span className="gridCard-title">Косметическая промышленность</span></a>
-                </div>
-                <div className="gridCard-item">
+    return (
+        <div>
+            <div >
+                <div className="gridCard-items">
+                    <div className="gridCard-item">
+                        <img className="gridCard-img" src={img1} alt="Косметика" />
+                        <a className="gridCard-item__link" href="http://212.233.79.177:6688/" target='_blanket' rel="noopener noreferrer"><span className="gridCard-title">Косметическая промышленность</span></a>
+                    </div>
+                    <div className="gridCard-item">
                         <img className="gridCard-img" src={img2} alt="Фарма" />
-                    <a className="gridCard-item__link" href="http://212.233.79.177:6688/" target='_blanket' rel="noopener noreferrer"><span className="gridCard-title">Фармацевтика</span></a>
-                </div>
-                <div className="gridCard-item">
-                    <img className="gridCard-img" src={img3} alt="Бытовая химия" />
-                    <a className="gridCard-item__link" href="http://212.233.79.177:6688/" target='_blanket' rel="noopener noreferrer"><span  className="gridCard-title">Бытовая химия</span ></a>
-                </div>
-                <div className="gridCard-item">
-                    <img className="gridCard-img" src={img4} alt="Красители" />
-                    <a className="gridCard-item__link" href="http://212.233.79.177:6688/" target='_blanket' rel="noopener noreferrer"><span  className="gridCard-title">Краски и красители</span ></a>
-                </div>
-                <div className="gridCard-item">
-                    <img className="gridCard-img" src={img5} alt="Продукты и БАД-ы" />
-                    <a className="gridCard-item__link" href="http://212.233.79.177:6688/" target='_blanket' rel="noopener noreferrer"><span  className="gridCard-title">Продукты и БАД</span ></a>
-                </div>
-                <div className="gridCard-item">
-                    <img className="gridCard-img" src={img6} alt="Сельское хозяйство" />
-                    <a className="gridCard-item__link" href="http://212.233.79.177:6688/" target='_blanket' rel="noopener noreferrer"><span  className="gridCard-title">Сельское хозяйство</span ></a>
-                </div>
-                <div className="gridCard-item">
-                    <img className="gridCard-img" src={img7} alt="Строительство" />
-                    <a className="gridCard-item__link" href="http://212.233.79.177:6688/" target='_blanket' rel="noopener noreferrer"><span  className="gridCard-title" >Строительство</span ></a>
-                </div>
-                <div className="gridCard-item">
-                    <img className="gridCard-img" src={img8} alt="Энергетика" />
-                    <a className="gridCard-item__link" href="http://212.233.79.177:6688/" target='_blanket' rel="noopener noreferrer"><span  className="gridCard-title">Энергетика</span ></a>
-                </div>
-                <div className="gridCard-item">
-                    <img className="gridCard-img" src={img9} alt="Текстильная промышленность" />
-                    <a className="gridCard-item__link" href="http://212.233.79.177:6688/" target='_blanket' rel="noopener noreferrer"><span  className="gridCard-title">Текстильная промышленность</span ></a>
-                </div>
-                <div className="gridCard-item">
-                    <img className="gridCard-img" src={img10} alt="Машиностроение" />
-                    <a className="gridCard-item__link" href="http://212.233.79.177:6688/" target='_blanket' rel="noopener noreferrer"><span  className="gridCard-title">Машиностроение</span ></a>
-                </div>
-                <div className="gridCard-item">
-                    <img className="gridCard-img" src={img11} alt="Фотоэлементы" />
-                    <a className="gridCard-item__link" href="http://212.233.79.177:6688/" target='_blanket' rel="noopener noreferrer"><span  className="gridCard-title" >Фотоэлементы</span ></a>
-                </div>
-                <div className="gridCard-item">
-                    <img className="gridCard-img" src={img12} alt="Электротехника"/>
-                    <a className="gridCard-item__link" href="http://212.233.79.177:6688/" target='_blanket' rel="noopener noreferrer"><span className="gridCard-title">Электроника и электротехника</span ></a>
+                        <a className="gridCard-item__link" href="http://212.233.79.177:6688/" target='_blanket' rel="noopener noreferrer"><span className="gridCard-title">Фармацевтика</span></a>
+                    </div>
+                    <div className="gridCard-item">
+                        <img className="gridCard-img" src={img3} alt="Бытовая химия" />
+                        <a className="gridCard-item__link" href="http://212.233.79.177:6688/" target='_blanket' rel="noopener noreferrer"><span className="gridCard-title">Бытовая химия</span ></a>
+                    </div>
+                    <div className="gridCard-item">
+                        <img className="gridCard-img" src={img4} alt="Красители" />
+                        <a className="gridCard-item__link" href="http://212.233.79.177:6688/" target='_blanket' rel="noopener noreferrer"><span className="gridCard-title">Краски и красители</span ></a>
+                    </div>
+                    <div className="gridCard-item">
+                        <img className="gridCard-img" src={img5} alt="Продукты и БАД-ы" />
+                        <a className="gridCard-item__link" href="http://212.233.79.177:6688/" target='_blanket' rel="noopener noreferrer"><span className="gridCard-title">Продукты и БАД</span ></a>
+                    </div>
+                    <div className="gridCard-item">
+                        <img className="gridCard-img" src={img6} alt="Сельское хозяйство" />
+                        <a className="gridCard-item__link" href="http://212.233.79.177:6688/" target='_blanket' rel="noopener noreferrer"><span className="gridCard-title">Сельское хозяйство</span ></a>
+                    </div>
+                    <div className="gridCard-item">
+                        <img className="gridCard-img" src={img7} alt="Строительство" />
+                        <a className="gridCard-item__link" href="http://212.233.79.177:6688/" target='_blanket' rel="noopener noreferrer"><span className="gridCard-title" >Строительство</span ></a>
+                    </div>
+                    <div className="gridCard-item">
+                        <img className="gridCard-img" src={img8} alt="Энергетика" />
+                        <a className="gridCard-item__link" href="http://212.233.79.177:6688/" target='_blanket' rel="noopener noreferrer"><span className="gridCard-title">Энергетика</span ></a>
+                    </div>
+                    <div className="gridCard-item">
+                        <img className="gridCard-img" src={img9} alt="Текстильная промышленность" />
+                        <a className="gridCard-item__link" href="http://212.233.79.177:6688/" target='_blanket' rel="noopener noreferrer"><span className="gridCard-title">Текстильная промышленность</span ></a>
+                    </div>
+                    <div className="gridCard-item">
+                        <img className="gridCard-img" src={img10} alt="Машиностроение" />
+                        <a className="gridCard-item__link" href="http://212.233.79.177:6688/" target='_blanket' rel="noopener noreferrer"><span className="gridCard-title">Машиностроение</span ></a>
+                    </div>
+                    <div className="gridCard-item">
+                        <img className="gridCard-img" src={img11} alt="Фотоэлементы" />
+                        <a className="gridCard-item__link" href="http://212.233.79.177:6688/" target='_blanket' rel="noopener noreferrer"><span className="gridCard-title" >Фотоэлементы</span ></a>
+                    </div>
+                    <div className="gridCard-item">
+                        <img className="gridCard-img" src={img12} alt="Электротехника" />
+                        <a className="gridCard-item__link" href="http://212.233.79.177:6688/" target='_blanket' rel="noopener noreferrer"><span className="gridCard-title">Электроника и электротехника</span ></a>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default IndustryCard;
