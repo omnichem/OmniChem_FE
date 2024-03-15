4aaa52fe9b587b8d86c34eaf4250648684edc687
Oksana-Nad <o.nadtochiy@omnichem.ru>
2024-03-15 12:42:50 +0300

    test landing

--- src/pages/landingPage/header/Header.tsx
+++ src/pages/landingPage/header/Header.tsx
@@ -1,21 +1,33 @@
-import './header.css';
-
-import omnichemLogo from '../logo v3.png'
-
+import "./header.css";
+import { Divider } from "antd";
+import omnichemLogo from "../logo v3.png";
 
 const Header = () => {
   return (
-
     <div className="header">
       <div className="header-container">
-        <img className="header-logo" src={omnichemLogo} alt="omnichem логотип" />
-        <div className="navList" >
-          <a className="navList-link" href="/materials" target='_blanket' rel="noopener noreferrer">Для поставщиков</a>
-          <a className="navList-link" href="/materials" target='_blanket' rel="noopener noreferrer">Вход</a>
+        <div className="header-items">
+          <img
+            className="header-logo"
+            src={omnichemLogo}
+            alt="omnichem логотип"
+          />
+          <div className="navList">
+            {/* <a className="navList-link" href="/materials" target='_blanket' rel="noopener noreferrer">Для поставщиков</a> */}
+            <a
+              className="navList-link"
+              href="http://212.233.79.177/materials"
+              target="_blanket"
+              rel="noopener noreferrer"
+            >
+              Вход
+            </a>
+          </div>
         </div>
+        <Divider className="divider" />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Header;
