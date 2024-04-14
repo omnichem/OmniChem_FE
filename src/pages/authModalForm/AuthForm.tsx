aeb01bb66b4b063e5c9e517c26369ad76daaa921
Solnyshko-eto-ya <true.solnyshko@gmail.com>
2024-04-14 16:59:09 +0300

    Почти закончена авторизация

--- src/pages/authModalForm/AuthForm.tsx
+++ src/pages/authModalForm/AuthForm.tsx
@@ -5,10 +5,12 @@ import styled from 'styled-components';
 import { LoginForm } from './LoginForm';
 import { RegisterForm } from './RegisterForm';
 import { Logo } from '../../shared/components';
+import { useAuth } from '../../contexts/authContext';
 
 interface AuthFormProps {}
 
 export const AuthForm: React.FC<AuthFormProps> = ({}) => {
+  const { isAuthorized } = useAuth();
   const [items] = useState([
     {
       key: '1',
@@ -22,11 +24,20 @@ export const AuthForm: React.FC<AuthFormProps> = ({}) => {
     },
   ]);
   return (
-    <AuthFormWrapper vertical>
+    <AuthFormWrapper vertical align="center">
       <Logo width={300} height={170} />
-      <h2 style={{ fontSize: '27px' }}>Добро пожаловать в OmniChem!</h2>
-      <p>Выберите, что вы хотите сделать:</p>
-      <Tabs defaultActiveKey="1" type="card" size={'large'} items={items} />
+      {isAuthorized ? (
+        <Flex align="center" vertical>
+          <h2>Вы успешно вошли</h2>
+          <p>Теперь вы можете закрыть это окно и продолжить пользоваться сайтом</p>
+        </Flex>
+      ) : (
+        <Flex vertical align="center" style={{ width: '100%' }} gap={10}>
+          <h2 style={{ fontSize: '27px' }}>Добро пожаловать в OmniChem!</h2>
+          <p>Выберите, что вы хотите сделать:</p>
+          <Tabs defaultActiveKey="1" type="card" size={'large'} items={items} />
+        </Flex>
+      )}
     </AuthFormWrapper>
   );
 };
